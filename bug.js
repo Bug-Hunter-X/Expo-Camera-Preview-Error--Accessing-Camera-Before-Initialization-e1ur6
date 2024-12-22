69856252788708de2031433f745d1265afd786ba
BugHunter-X <askzenai@gmail.com>
2024-12-22 11:10:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo Camera API and trying to access the camera's preview before it's fully initialized. This typically happens when you try to manipulate the camera preview (e.g., taking a picture, changing camera type) immediately after mounting the component that uses the Camera API.
